74d0a477b0da7bc594438d35508b1a4df69f8d71
magda.oleksy <oleksy.magda@gmail.com>
2017-10-25 20:21:01 +0200

    Commiting gitTutorial prpjct

---
+++ gitTutorial/src/gitTutorial/HelloGit.java
@@ -0,0 +1,15 @@
+package gitTutorial;
+
+public class HelloGit {
+
+	public HelloGit() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Git");
+
+	}
+
+}
